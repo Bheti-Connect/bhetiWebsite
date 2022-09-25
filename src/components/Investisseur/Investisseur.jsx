1723ac6686fa0724e5a46b9667ca62545c623927
Banitongwa7 <davidbanitongwa@gmail.com>
2022-09-25 13:27:13 +0100

    add headertext in investisseur

--- src/components/Investisseur/Investisseur.jsx
+++ src/components/Investisseur/Investisseur.jsx
@@ -1,17 +1,42 @@
 import styled from 'styled-components'
 import NavBar from '../NavBar';
+import { useTheme } from '../../context/themeContext';
 
 const Investisseur = () => {
+
+    const theme = useTheme()
+
     return (
         <InvestisseurStyled>
-            <div>
-                <h1>COMPONENT PAGE INVESTISSEUR</h1>
-            </div>
+            <HeaderText theme={theme} >
+            <h3>Découvrez des opportunités d'investissements exclusives</h3>
+            <p>Vous trouverez ci-dessous des informations clés sur des startups et PME qui ouvrent leur capital pour prendre une position décisive sur leurs
+            marchés.<br/>Vous souhaitez en savoir plus sur ces opportunités et/ou rencontrer les fondateurs ? Cliquez sur "Recevoir le deck".</p>
+            </HeaderText>
         </InvestisseurStyled>
     )
 }
 
+// Style CSS with styled component
+
 const InvestisseurStyled = styled.section``;
+
+const HeaderText = styled.div`
+margin: 55px;
+
+h3 {
+  
+  font-size: 25px;
+  color: ${props => props.theme.colorPrimary};
+  
+}
+
+p {
+  margin: 10px 0;
+  font-size: 14px;
+}
+
+`;
 
 
 export default Investisseur
