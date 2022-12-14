8d548d56a12df1cbc163bbc56252374f67cb1603
Umer Sohail <mr.umersohail@gmail.com>
2022-12-14 20:15:11 +0500

    routes case sensitive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { useTheme } from './context/themeContext';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Entrepreneur from './components/Entrepreneur/Entrepreneur';
 import Investisseur from './components/Investisseur/Investisseur';
-import {AccountBox} from './components/Auth/accountBox';
+import { AccountBox } from './components/Auth/accountBox';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
@@ -20,34 +20,46 @@ const App = () => {
   const theme = useTheme();
 
   const [isAuthenticated, setIsAuthenticated] = useState();
-  
+
   return (
     <Router>
       <AppStyled theme={theme}>
-          <div>
-            <NavBar />
-          </div>
+        <div>
+          <NavBar />
+        </div>
         <main>
           <Routes>
-            <Route path='/bhetiConnectProj' exact element={<Home />} />
-            <Route path='entrepreneur' exact element={<Entrepreneur />} />
-            <Route path='investisseur' exact element={<Investisseur />} />
-            <Route path='media' exact element={<Media />} />
-            <Route path='connexion' exact element={<AccountBox />} />
+            {/* <Route
+              exact
+              strict
+              caseSensitive={false}
+              path='/:url([a-z/]*[A-Z]+[a-z/]*)/'
+              element={props => {
+                const path = props.location.pathname
+                alert('ok');
+                // return <Redirect to={`${path.toLowerCase()}`} />
+              }}
+            /> */}
+            <Route path='/bheti-connect-proj' caseSensitive={false} render={() => {
+            }} exact element={<Home />} />
+            <Route path='entrepreneur' exact caseSensitive={false} element={<Entrepreneur />} />
+            <Route path='investisseur' exact caseSensitive={false} element={<Investisseur />} />
+            <Route path='media' exact caseSensitive={false} element={<Media />} />
+            <Route path='connexion' exact caseSensitive={false} element={<AccountBox />} />
             {/* <Route path='evaluer-eligibilite' element={<EvaluerEligibilite />} /> */}
-            <Route path='pitch-deck' exact element={<FormOne />} />
-            <Route path='evaluer-eligibilite' exact element={<FormTwo />} />
+            <Route path='pitch-deck' exact caseSensitive={false} element={<FormOne />} />
+            <Route path='evaluer-eligibilite' exact caseSensitive={false} element={<FormTwo />} />
 
             {/* Route : form investisseur */}
-            <Route path='forminvestisseur' exact element={<FormInvestisseur />}/>
+            <Route path='form-investisseur' exact caseSensitive={false} element={<FormInvestisseur />} />
             {/* Route : success stories media */}
-            <Route path='success-stories' exact element={<SuccessStories />}/>
-            <Route path='' element={<ErrorPage />} />
+            <Route path='success-stories' exact caseSensitive={false} element={<SuccessStories />} />
+            <Route path='*' element={<ErrorPage />} />
           </Routes>
         </main>
-          <div>
-            <Footer />
-          </div>
+        <div>
+          <Footer />
+        </div>
       </AppStyled>
     </Router>
   )
