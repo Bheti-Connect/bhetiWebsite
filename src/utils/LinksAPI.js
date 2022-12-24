4799298dcca7419b7e4648cac66c0f0e2a626878
Banitongwa David <davidbanitongwa@gmail.com>
2022-12-24 18:41:37 +0100

    add LinksAPI file

---
+++ src/utils/LinksAPI.js
@@ -0,0 +1,13 @@
+const LinksAPI = {
+    projets: "https://bheti-connect.smirltech.com/api/projets",
+    projetsPage: function(page){
+        return `https://bheti-connect.smirltech.com/api/projets?page=${page}`
+    },
+    projetsSearch: "https://bheti-connect.smirltech.com/api/projets/search",
+    projetsSearchPage: function(page){
+        return `https://bheti-connect.smirltech.com/api/projets/search?page=${page}`
+    },
+    
+}
+
+export default LinksAPI
