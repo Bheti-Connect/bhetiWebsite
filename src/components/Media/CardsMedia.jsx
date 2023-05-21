869f46fcde6eb3be46f85e0eaa1981aec2f543a6
Banitongwa David <davidbanitongwa@gmail.com>
2023-05-21 16:10:19 +0100

    interviews

--- src/components/Media/CardsMedia.jsx
+++ src/components/Media/CardsMedia.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTheme } from '../../context/themeContext';
 import styled from 'styled-components';
 import { handleSelect } from '../../utils/FunctionsComponent';
 
 const CardsMedia = ({item, setSelect, setModal}) => {
 
+  // Generate color for background
+  const [backColor, setBackColor] = useState("rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")");
+
+  // Generate image API
+  let source = 
+  
   const theme = useTheme()
-
-
-   // Generate image
-   let source = "https://" + `picsum.photos/id/${item.id}/200/300`;
 
 
   return (
@@ -20,11 +22,18 @@ const CardsMedia = ({item, setSelect, setModal}) => {
         </CardHeader>
 
         <CardBody theme={theme}>
-        <h3>{item.poste}</h3>
+
+        <div className='boxTitle'>
+          <img src={item.image} alt='icon media'/>
+          <h3>{item.title}</h3>
+        </div>
 
         <ul>
-          <li>{item.secteur}</li>
-          <li>{item.pays}</li>
+          {
+            item.secteurs.map((secteur,  index) => (
+              <li key={index} title='Secteurs' style={{backgroundColor: `${backColor}`}}>{secteur}</li>
+            ))
+          }
         </ul>
         
         </CardBody>
@@ -41,9 +50,9 @@ border-radius: 10px;
 box-shadow: 0 2px 20px rgba(0, 0, 0, 0.2);
 overflow: hidden;
 width: 360px;
-height: 330px;
+height: auto;
 cursor: pointer;
-background-color: ${props => props.theme.colorBheti};
+background-color: ${props => props.theme.body};
 
 &:hover{
   transition: transform 0.2s ease-in-out;
@@ -62,32 +71,35 @@ h3{
   font-family: 'Inter', sans-serif;
   font-weight: 700;
   font-size: 15px;
-  color:white;
+  color: ${props => props.theme.colorBheti};
   line-height: 18px;
   margin-bottom: 13px;
 }
 
 ul{
   display: flex;
-  justify-content: left;
   list-style:none;
   font-size: 12px;
-  margin-top: 40px;
   padding:0;
 }
 
 ul li {
   margin-right:8px;
-  background-color: white;
-  color: ${props => props.theme.colorBheti};
-  border-radius: 10px;
-  padding:5px 5px;
-  font-family: 'Inter', sans-serif;
-  font-weight:600;
-  /* tronque text  */
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
+  margin-top: 4px;
+  color: white;
+  border-radius: 6px;
+  padding:2px 9px;
+ 
+}
+
+.boxTitle{
+  display: flex;
+  img{
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    object-fit: cover;
+  }
 }
 
 `;
@@ -98,7 +110,7 @@ img {
   width: 100%;
   height: 220px;
   object-fit: cover;
-  border-radius: 10px;
+  border-radius: 10px 10px 0 0;
 }
 
 `;
